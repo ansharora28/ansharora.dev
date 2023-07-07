81c0ad0516f7bf68d9275aa63f02091c59ae9c9e
Ansh <ansharorake2@gmail.com>
2023-07-07 13:46:17 +0530

    deploy to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
+  site: 'https://ansharora28.github.io',
+  base: '/ansharora.dev',
 });
